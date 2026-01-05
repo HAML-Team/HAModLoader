3c135a9b1feed577d92ff2990c86d9b1cf717317
Trident35ro <107998245+Trident35ro@users.noreply.github.com>
2026-01-05 22:42:00 +0200

    Updated HAMod

--- Assets/Scripts/HAModLoader/API/HAMod.cs
+++ Assets/Scripts/HAModLoader/API/HAMod.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 namespace HAModLoaderAPI
 {
     public interface HAMod
     {
-        public string ModName => GetType().Name;
-        public string ModAuthor => GetType().Name;
-
+        public string ModName { get; }
+        public string ModAuthor { get; }
+        public string ModVersion { get; }
+        public Sprite ModLogo { get; }
+        public string ModDescriptionShort => null;
         public void OnModLoad() { }
         public void OnEnterScene(Scene scene) { }
         public void OnCreate(GameObject obj) { }
